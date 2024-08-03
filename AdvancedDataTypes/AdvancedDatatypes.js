fb95a944b4b3f3adf30f6407a4882be089cd9a1a
Bishwajit Paul <paul.bishwajit09@gmail.com>
2024-08-03 17:09:14 +0600

    The file is committed

--- AdvancedDataTypes/AdvancedDatatypes.js
+++ AdvancedDataTypes/AdvancedDatatypes.js
@@ -2,6 +2,40 @@
 // Topics to cover Map, Set, weakMap, weakSet
 // ==========================================
 
+// Object
+// Description: The fundamental data type used to store collections of data and more complex entities.
+// Example:
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  fullName: function () {
+    return (
+      this.firstName + " " + this.lastName + "is " + this.age + " years old."
+    );
+  },
+};
+console.log(person.fullName());
+
+// Array
+// Description: A special type of object used for storing ordered collections of items.
+// Example:
+const numbers = [1, 2, 3, 4, 5];
+
+// Function
+// Description: Functions are regular objects with callable functionality.
+// Example:
+function greet() {
+  console.log("Hello, World!");
+}
+greet();
+
+// Date
+// Description: An objet to work dates and times.
+// Example:
+const now = new Date();
+console.log("Now it is:", now);
+
 // Map :
 //Description: A collection of key - value pairs where keys can be any type.
 // Example:
@@ -33,8 +67,27 @@ console.log("The object is:", weakmap.get(obj));
 // WeakSet
 // Description: Similar to Set but values must be objects and they do not prevent garbage
 // Example:
-
 const weakSet = new WeakSet();
 const object = { name: "Book" };
 weakSet.add(object);
 console.log("The object is: ", object.name);
+
+// Typed Arrays
+// Description: Arrays for handling binary data (like buffers)
+// Example:
+const buffer = new ArrayBuffer(24);
+const view = new Uint8Array(buffer);
+view[0] = 255;
+console.log("The array buffer result is: ", view);
+
+// BigInteger
+// Description: A numeric data type that can represent integers with arbitrary precision.
+// Example:
+const bigInteger = 1234567890123456789012345678901234567890n;
+console.log("The big integer is: ", bigInteger);
+
+// Symbol
+// Description: A unique and immutable primitive value often used to identify object properties.
+// Example:
+const sym = Symbol("Bird");
+console.log("The symbol is:", sym);
